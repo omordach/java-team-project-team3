00f72fc6736271ecce16c646b035d79a78419652
Oleg Mordach <omordach@gmail.com>
2018-05-13 15:57:48 +0300

    Creted new class TestParsing (just template, without test body).

---
+++ src/test/java/TestParsing.java
@@ -0,0 +1,12 @@
+import org.testng.annotations.Test;
+
+
+public class TestParsing {
+    @Test
+    public void testParsing() {
+
+        /* Here should be test body*/
+        System.out.println("Test for parsing");
+
+    }
+}
